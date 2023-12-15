5f27de3010644e2a233917615b62cf0efab22341
José Evandro Rodrigues Filho <zedepoa@gmail.com>
2023-12-15 01:25:24 -0300

    Finalizando componente de sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -18,14 +18,19 @@ export class SidebarComponent {
   faMoneyBillWave = faMoneyBillWave;
 
   menus: any[] = [
-    { id: 1, icon: faHome, nome: "Empresa", link: "/about", visibleProfiles: ["Estabelecimento", "Federação", "Time", "Atleta"] },
-    { id: 2, icon: faUserPlus, nome: "Colaboradores", link: "/collaborators", visibleProfiles: ["Time", "Atleta"] },
-    { id: 3, icon: faUsers, nome: "Clientes", link: "/customers", visibleProfiles: ["Estabelecimento", "Federação", "Time", "Atleta"] },
-    { id: 4, icon: faShoppingCart, nome: "Compra", link: "/purchase", visibleProfiles: ["Estabelecimento", "Federação", "Time", "Atleta"] },
-    { id: 5, icon: faMoneyBillWave, nome: "Venda", link: "/sale", visibleProfiles: ["Estabelecimento", "Federação", "Time", "Atleta"] },
-    { id: 6, icon: faStore, nome: "Produtos", link: "/products", visibleProfiles: ["Estabelecimento", "Federação", "Time", "Atleta"] },
-    { id: 7, icon: faCity, nome: "Fornecedores", link: "/suppliers", visibleProfiles: ["Time", "Atleta"] },
-    // { id: icon: "", 8, nome: "Métricas", link: "/metricas", visibleProfiles: ["Estabelecimento"] },
-    { id: 9, icon: faDolly, nome: "Estoque", link: "/stock", visibleProfiles: ["Estabelecimento", "Federação", "Time", "Atleta"] }
+    { id: 1, class: "active", icon: faHome, nome: "Empresa", link: "/about", visibleProfiles: ["Estabelecimento", "Federação", "Time", "Atleta"] },
+    { id: 2, class: "", icon: faUserPlus, nome: "Colaboradores", link: "/collaborators", visibleProfiles: ["Time", "Atleta"] },
+    { id: 3, class: "", icon: faUsers, nome: "Clientes", link: "/customers", visibleProfiles: ["Estabelecimento", "Federação", "Time", "Atleta"] },
+    { id: 4, class: "", icon: faShoppingCart, nome: "Compra", link: "/purchase", visibleProfiles: ["Estabelecimento", "Federação", "Time", "Atleta"] },
+    { id: 5, class: "", icon: faMoneyBillWave, nome: "Venda", link: "/sale", visibleProfiles: ["Estabelecimento", "Federação", "Time", "Atleta"] },
+    { id: 6, class: "", icon: faStore, nome: "Produtos", link: "/products", visibleProfiles: ["Estabelecimento", "Federação", "Time", "Atleta"] },
+    { id: 7, class: "", icon: faCity, nome: "Fornecedores", link: "/suppliers", visibleProfiles: ["Time", "Atleta"] },
+    // { id: class:"", icon: "", 8, nome: "Métricas", link: "/metricas", visibleProfiles: ["Estabelecimento"] },
+    { id: 9, class: "", icon: faDolly, nome: "Estoque", link: "/stock", visibleProfiles: ["Estabelecimento", "Federação", "Time", "Atleta"] }
   ]
+
+  onChangeActive(event: any) {
+    document.getElementsByClassName('active')[0].classList.remove('active');
+    document.getElementById(event)?.classList.add('active');
+  }
 }
