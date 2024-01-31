8a6d5c39ed5cb031ff6083c74a59cd850da37daa
José Evandro Rodrigues Filho <zedepoa@gmail.com>
2024-01-31 02:13:25 -0300

    Creating the customer detail component

---
+++ src/app/modules/customers/components/customer-detail/customer-detail.component.ts
@@ -0,0 +1,76 @@
+import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-customer-detail',
+  templateUrl: './customer-detail.component.html',
+  styleUrls: ['./customer-detail.component.css']
+})
+export class CustomerDetailComponent {
+
+
+
+  @Input() id!: number;
+  formGroup!: FormGroup;
+  buttons = [
+    {
+      name: 'VOLTAR',
+      link: '/customers',
+      class: 'btn-secondary'
+    },
+    {
+      name: 'SALVAR',
+      link: '/customerDetail/',
+      class: 'btn-primary'
+    }]
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.formGroup = this.formBuilder.group({
+      name: '',
+      taxNumber: '',
+      corporateReason: '',
+      cnae: '',
+      stateRegistration: '',
+      creationDate: '',
+      situationRevenue: '',
+      street: '',
+      city: '',
+      estate: '',
+      country: '',
+      zipCode: '',
+      complement: '',
+      homePhone: '',
+      cellPhone: '',
+      email: '',
+      TotalCollaborators: '',
+      sunday: '',
+      monday: '',
+      tuesday: '',
+      wednesday: '',
+      thursday: '',
+      friday: '',
+      saturday: '',
+      sundayStart: '',
+      mondayStart: '',
+      tuesdayStart: '',
+      wednesdayStart: '',
+      thursdayStart: '',
+      fridayStart: '',
+      saturdayStart: '',
+      sundayEnd: '',
+      mondayEnd: '',
+      tuesdayEnd: '',
+      wednesdayEnd: '',
+      thursdayEnd: '',
+      fridayEnd: '',
+      saturdayEnd: '',
+    })
+  }
+
+  ngOnSubmit() {
+
+  }
+}
