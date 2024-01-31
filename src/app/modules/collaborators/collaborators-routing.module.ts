7a54104961f9b4e3093139cadb60e5195f0f1b30
José Evandro Rodrigues Filho <zedepoa@gmail.com>
2024-01-31 02:15:21 -0300

    Creation of customer routing

--- src/app/modules/collaborators/collaborators-routing.module.ts
+++ src/app/modules/collaborators/collaborators-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ThemeComponent } from '../theme/theme.component';
+import { ThemesComponent } from '../themes/themes.component';
 import { CollaboratorsComponent } from './collaborators.component';
 import { CollaboratorDetailComponent } from './components/collaborator-detail/collaborator-detail.component';
 
 const collaboratorsRoutes: Routes = [
   {
     path: '',
-    component: ThemeComponent,
+    component: ThemesComponent,
     children: [
       { path: '', redirectTo: 'collaborators', pathMatch: 'full' },
       { path: 'collaborators', component: CollaboratorsComponent },
