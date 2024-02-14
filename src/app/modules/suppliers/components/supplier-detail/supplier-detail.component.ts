10a9275b4a50a30718430b035a9ca8c54749fba9
Keven Costa <kevencosta129@gmail.com>
2024-02-14 09:10:35 -0300

    Supplier validation fields

--- src/app/modules/suppliers/components/supplier-detail/supplier-detail.component.ts
+++ src/app/modules/suppliers/components/supplier-detail/supplier-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, RequiredValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-supplier-detail',
@@ -19,7 +19,7 @@ export class SupplierDetailComponent {
     name: 'SALVAR',
     link: '/supplierDetail/',
     class: 'btn-primary'
-  }] 
+  }]
 
   constructor(private formBuilder: FormBuilder) {
   }
@@ -40,6 +40,60 @@ export class SupplierDetailComponent {
       nameUser: '',
       telephone: '',
       email: ''
-    })
+    });
+    this.formGroup.controls["name"].addValidators([Validators.required, Validators.minLength(5)]);
+    this.formGroup.controls["cnpj"].addValidators([Validators.required, Validators.minLength(13)]);
+    this.formGroup.controls["telephone"].addValidators([Validators.required]);
+    this.formGroup.controls["email"].addValidators([Validators.required]);
+
+  }
+  get name(){
+    return this.formGroup.get("name")!;
+  }
+  get cnpj(){
+    return this.formGroup.get("cnpj")!;
+  }
+  get stateRegistration(){
+    return this.formGroup.get("stateRegistration")!;
+  }
+  get branch(){
+    return this.formGroup.get("branch")!;
+  }
+  get street(){
+    return this.formGroup.get("street")!;
+  }
+  get numberStreet(){
+    return this.formGroup.get("numberStreet")!;
+  }
+  get city(){
+    return this.formGroup.get("city")!;
+  }
+  get estate(){
+    return this.formGroup.get("estate")!;
+  }
+  get country(){
+    return this.formGroup.get("country")!;
+  }
+  get zipCode(){
+    return this.formGroup.get("zipCode")!;
+  }
+  get complement(){
+    return this.formGroup.get("complement")!;
+  }
+  get nameUser(){
+    return this.formGroup.get("nameUser")!;
+  }
+  get telephone(){
+    return this.formGroup.get("telephone")!;
+  }
+  get email(){
+    return this.formGroup.get("email")!;
+  }
+
+  ngOnSubmit(event:any){
+    if(this.formGroup.invalid){
+      return;
+    }
+    console.log("submit");
   }
 }
